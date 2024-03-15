186039764a634f3076fd11f06ff401b89353574c
Renzo Paul Chamorro Rojas <renzo607@icloud.com>
2024-03-15 11:16:30 -0500

    Initial Commit

---
+++ bancom/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  bancom
+//
+//  Created by Renzo Paul Chamorro on 15/03/24.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
